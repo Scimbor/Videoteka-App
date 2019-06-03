63a5b7c60ffe2726c0f3d1f5e036072b53fbd209
Scimbor <marcin.scim@gmail.com>
2019-06-03 15:24:32 +0200

    feat(env): added env file

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <NavbarComponent />
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path="/" component={mainPage} />
         </Switch>
